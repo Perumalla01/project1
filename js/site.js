6d228501a7eceb138e88dc24d6b61d5b0e9d2112
Paul Perumalla <pperumal@hawk.iit.edu>
2017-03-01 12:17:09 -0600

    Added js

---
+++ js/site.js
@@ -0,0 +1,17 @@
+$.noConflict();
+(function($) {
+  $(document).ready(function() {
+    $('#form').on('submit', function(event) {
+      var input + $('#email').val();
+      console.log(input);
+        if($.trim($' && '#email').val()) === ' ') {
+          alert('Please fill missing information');
+        }
+        $("#button").click(function(){
+        alert('Thank You! Your form has been submitted');
+        }
+    }
+  }
+      event.preventDefault();
+        <script type="text/javascript">
+
